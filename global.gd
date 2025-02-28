2dae8985f659749ddfb93919fd7c3d81ec1a403f
Aglo <113240095+Muminwilmer@users.noreply.github.com>
2025-02-28 18:08:59 +0100

    some stuff

--- global.gd
+++ global.gd
@@ -1,3 +1,3 @@
 extends Node
 
-var oldTime := true
+var oldTime := false
--- player.gd
+++ player.gd
@@ -1,8 +1,9 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+const walkspeed = 180.0
+#const runspeed = 240
+const jumpforce = -300.0
 
 
 @onready var newtime_map = get_parent().get_node("Newtime")
@@ -17,22 +18,12 @@ func _physics_process(delta):
 		velocity.y += gravity * delta
 
 	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+		velocity.y = jumpforce
 
 	var direction = Input.get_axis("left", "right")
 	if direction:
-		velocity.x = direction * SPEED
+		velocity.x = direction * walkspeed
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.x = move_toward(velocity.x, 0, walkspeed)
 	move_and_slide()
 
-	if Input.is_action_just_pressed("timeswap"):
-		Global.oldTime = !Global.oldTime
-		toggle_time()
-
-func toggle_time():
-	newtime_map.visible = not Global.oldTime
-	oldtime_map.visible = Global.oldTime
-	
-	oldtime_map.set_process(Global.oldTime)
-	newtime_map.set_process(not Global.oldTime)
