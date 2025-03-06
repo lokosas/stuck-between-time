f5c58efcbf1abca465961ab6fdbec019419ac066
Aglo <113240095+Muminwilmer@users.noreply.github.com>
2025-03-06 21:46:21 +0100

    updated to latest version

--- Assets/Scripts/LevelScripts/level_1.gd
+++ Assets/Scripts/LevelScripts/level_1.gd
@@ -1,9 +1,5 @@
 extends Node2D
 
-
-@onready var og_newtime_pos = $Newtime.position
-@onready var og_oldtime_pos = $Oldtime.position
-
 func _ready():
 	toggle_time()
 	
@@ -15,10 +11,5 @@ func _process(delta):
 func toggle_time():
 	$Newtime.visible = not Global.oldTime
 	$Oldtime.visible = Global.oldTime
-	
-	if Global.oldTime:
-		$Newtime.position.y = -2000
-		$Oldtime.position.y = og_newtime_pos.y  # Bring the new map back
-	else:
-		$Oldtime.position.y = -2000
-		$Newtime.position.y = og_oldtime_pos.y
+	$Newtime.collision_enabled = not Global.oldTime
+	$Oldtime.collision_enabled = Global.oldTime
