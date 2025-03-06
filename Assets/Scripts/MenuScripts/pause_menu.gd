3c7e8a993a7063c2bbb14ec063a60046d3e1e5ba
Aglo <113240095+Muminwilmer@users.noreply.github.com>
2025-03-06 21:09:03 +0100

    pause works now or smth

--- Assets/Scripts/MenuScripts/pause_menu.gd
+++ Assets/Scripts/MenuScripts/pause_menu.gd
@@ -1,15 +1,33 @@
 extends Control
 
+func _ready():
+	unpause()
+
 func pause():
 	get_tree().paused = true
-
+	visible = true
+	
 func unpause():
 	get_tree().paused = false
-
+	visible = false
 
 func _input(event):
-	if event.is_action_pressed("esc") and get_tree().paused:
-		pause()
+	if event.is_action_pressed("esc"):
+		if get_tree().paused:
+			unpause()
+		else:
+			pause()
 
-	if event.is_action_pressed("esc") and get_tree().paused:
-		unpause()
+
+func _on_resume_pressed():
+	unpause()
+
+func _on_settings_pressed():
+	pass # Replace with function body.
+
+
+func _on_restart_pressed():
+	get_tree().reload_current_scene()
+
+func _on_quit_pressed():
+	get_tree().quit()
